8d6724dbde38acb81233576090d2f6de8cb2dbe8
aaza <dump.truck.aja@gmail.com>
2025-09-24 21:17:09 +0700

    Versi oke

--- quiz1/tourist/scripts.js
+++ quiz1/tourist/scripts.js
@@ -18,7 +18,7 @@ function fillTheList(data) {
                 <div>
                     <h3 class="font-heading fg-text" >${data[i].title}</h3>
                     <p class="font-body fg-text" >${data[i].desc.substring(0,100)}...</p>
-                    <a href="/quiz1/tourist/item/?id=${i}" class="highlighted-link"> Check_it_out</a>
+                    <a href="/tourist/item/?id=${i}" class="highlighted-link"> Check_it_out</a>
                 </div>
                 <div>
                     <img src='${data[i].img_src}' alt=''/>
@@ -33,7 +33,7 @@ function fillTheList(data) {
 
 
 $(document).ready(function () {
-    fetch('/quiz1/tourist/data.json').then((res) => {
+    fetch('/tourist/data.json').then((res) => {
         jsonres = res.json()
 
         return jsonres
