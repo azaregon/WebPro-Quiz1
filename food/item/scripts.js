658193d80a86ce019c6fe94d2eb3005e26183482
aaza <dump.truck.aja@gmail.com>
2025-09-24 21:07:30 +0700

    profile and food done yes?

---
+++ food/item/scripts.js
@@ -0,0 +1,28 @@
+
+$('#burger-icon-btn').on('change', function () {
+    let selected = $(this)
+    
+    if (selected.prop("checked")) {
+        $('#header-drop').show();
+    } else {
+        $('#header-drop').hide();
+    }
+});
+
+
+
+$(document).ready(function () {
+    let the_url = new URL(window.location.href)
+    id = the_url.searchParams.get('id')[0];
+    fetch('/food/data.json').then((res) => {
+        jsonres = res.json()
+
+        return jsonres
+    }).then((jsonres) => {
+        console.log(jsonres[id])
+        $('#desc').html(jsonres[id].desc);
+        $('#spot-name').text(jsonres[id].title);
+        $('#img_src').attr('src', jsonres[id].img_src)
+        // $(selector).text(textString);
+    })
+});
