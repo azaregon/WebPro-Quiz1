3ba8268741c6ca5dbaaf44c8539ec21141fb9c5d
aaza <dump.truck.aja@gmail.com>
2025-09-25 10:03:38 +0700

    restructure dir

---
+++ quiz1/food/scripts.js
@@ -0,0 +1,44 @@
+
+
+$('#burger-icon-btn').on('change', function () {
+    let selected = $(this)
+    
+    if (selected.prop("checked")) {
+        $('#header-drop').show();
+    } else {
+        $('#header-drop').hide();
+    }
+});
+
+function fillTheList(data) {
+    for (i=0 ; i < data.length ; i++ ) {
+        $('#spot-list').append(`
+            <!-- list-item-start -->
+            <div class="list-item">
+                <div>
+                    <h3 class="font-heading fg-text" >${data[i].title}</h3>
+                    <p class="font-body fg-text" >${data[i].desc.substring(0,100)}...</p>
+                    <a href="/quiz1/food/item/?id=${i}" class="highlighted-link"> Check_it_out</a>
+                </div>
+                <div>
+                    <img src='${data[i].img_src}' alt=''/>
+                </div>
+            </div>
+            <!-- list-item-end -->
+            `)
+
+        // $(selector).append(content);
+    }
+}
+
+
+$(document).ready(function () {
+    fetch('/quiz1/food/data.json').then((res) => {
+        jsonres = res.json()
+
+        return jsonres
+    }).then((jsonres) => {
+        console.log(jsonres)
+        fillTheList(jsonres)
+    })
+});
